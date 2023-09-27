e89f6fbd26c55b69e209f9c39fe9591f5f44b7ff
syazah <120459599+syazah@users.noreply.github.com>
2023-09-27 06:28:44 +0000

    INTERMEDIATE EVENT

--- 08_EVENT_LISTENERS/01_basic.js
+++ 08_EVENT_LISTENERS/01_basic.js
@@ -11,5 +11,10 @@
 /*  agar div ke andar ek element hai aur div and uske andar wale element dono par event listener laga hai to
     pehle andar wale element ka event propogate hoga fir bahar wale ka. Isko bubble propogation bhi bolte hain.
 
-    Agar isko ulta karna hai to capturing mode ko true karna hoga dono ke event listeners me
+    Agar isko ulta karna hai to capturing mode ko true karna hoga dono ke event listeners me.
+
+    Agar event ko propogate nahi karna matlab agar andar ke event ko click karne par hum ye nahi chahte ki upar ka element bhi click ho to hum e.stopPropogation ko use karenge
 */ 
+
+//----------------------------PREVENT DEFAULT --------------------------------------------------------------
+/*  To prevent the default behaviour of an element we can write e.preventDefault(), this is used inside event listener function */
